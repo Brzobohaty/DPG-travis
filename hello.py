5e9b1f67112078763d54d2032954fd9c19fea132
Jan Brzobohatý <jan.brzobohaty@post.cz>
2017-04-18 11:28:20 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import sys
 import multirocessing as mp
 
-print("Python version %s " str(sys.version))
-print("Cores          %s "str(mp.cpu_count()))
+print("Python version %s " % str(sys.version))
+print("Cores          %s " % str(mp.cpu_count()))
+
+if 1:
+  print("tady")
+else:
+  print("tady nikdy")
